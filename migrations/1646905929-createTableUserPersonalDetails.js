79a24c97b1226ccfe7084c0c1372cdfc7350dfcc
Nella1a <m.allen@gmx.at>
2022-03-10 11:06:06 +0100

    Add create table user_personal_details

---
+++ migrations/1646905929-createTableUserPersonalDetails.js
@@ -0,0 +1,21 @@
+exports.up = async (sql) => {
+  await sql`
+	CREATE TABLE user_personal_details(
+		id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		first_name varchar(30) NOT NULL,
+		last_name varchar(30) NOT NULL,
+		date_of_birth DATE NOT NULL,
+		social_sec_nb integer NOT NULL,
+		nationality varchar(30),
+		email varchar(320),
+		phone varchar(20) NOT NULL,
+		user_id integer REFERENCES users (id) ON DELETE CASCADE
+	)
+	`;
+};
+
+exports.down = async (sql) => {
+  await sql`
+		DROP TABLE user_personal_details
+`;
+};
